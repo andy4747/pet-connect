c1c4fb564af6761ae8a6a4425e93117cce2f4fe9
Angel Dhakal <angel.dhakal@rts.com.np>
2021-12-16 16:02:23 +0545

    Landing page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Hero } from './Hero';
 import { Navbar } from './Navbar';
 
 export const Home = () => {
 	return (
 		<>
 			<Navbar />
+			<Hero />
 		</>
 	);
 };
