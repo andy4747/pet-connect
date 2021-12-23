88a46f67e508a12bab1a10ccd433bbb82fae4692
Angel Dhakal <angel.dhakal@rts.com.np>
2021-12-23 18:53:33 +0545

    Navbar (Landing Page)✅✅

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Logo from '../public/dog2.svg';
+import { MobileNavbar } from './MobileNavbar';
 
 export const Navbar = () => {
+	const [hamState, setHamState] = useState(false);
 	return (
 		<header className='font-Poppins'>
 			<nav className='container flex items-center py-4 mt-4 sm:mt-12'>
@@ -13,25 +16,36 @@ export const Navbar = () => {
 				<h2 className='cursor-pointer ml-2 font-semibold text-2xl mb-2 text-pet-indigo'>
 					PetConnect
 				</h2>
-				<ul className='hidden sm:flex flex-1 justify-end items-center gap-12 text-black-100 uppercase text-xs mb-2'>
+				<ul className='hidden sm:flex flex-1 justify-end  md:gap-6 md:text-xs lg:text-sm lg:border-0 items-center gap-12 font-bold text-black-100 uppercase text-xs mb-2'>
 					<li className='cursor-pointer'>Home</li>
 					<li className='cursor-pointer'>About Us</li>
 					<li className='cursor-pointer'>Contact</li>
-					<button
-						className='cursor-pointer bg-pet-red text-pet-white rounded-md px-7 py-3 uppercase'
-						type='button'>
-						Login
-					</button>
-					<button
-						className='cursor-pointer bg-pet-indigo text-pet-white hover:bg-pet-blue transition duration-300 shadow-md rounded-md px-7 py-3 uppercase'
-						type='button'>
-						Register
-					</button>
+
+					<Link href='/login' passHref>
+						<button
+							className='cursor-pointer bg-pet-red text-pet-white rounded-md px-7 py-3 uppercase'
+							type='button'>
+							Login
+						</button>
+					</Link>
+					<Link href='/register' passHref>
+						<button
+							className='cursor-pointer bg-pet-indigo text-pet-white hover:bg-pet-blue transition duration-300 shadow-md rounded-md px-7 py-3 uppercase'
+							type='button'>
+							Register
+						</button>
+					</Link>
 				</ul>
 				<div className='flex sm:hidden flex-1 justify-end cursor-pointer'>
-					<GiHamburgerMenu size={42} />
+					<GiHamburgerMenu
+						size={42}
+						onClick={() => {
+							setHamState(!hamState);
+						}}
+					/>
 				</div>
 			</nav>
+			{hamState && <MobileNavbar />}
 		</header>
 	);
 };
