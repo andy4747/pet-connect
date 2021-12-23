d7dc22c396a7b3f26992f9ae1490f99508c9ef23
Angel Dhakal <angel.dhakal@rts.com.np>
2021-12-23 18:52:45 +0545

    Hero section (Landing Page)✅✅

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -5,7 +5,7 @@ import HeroImg from '../public/hero1.jpg';
 export const Hero = () => {
 	return (
 		<section className='relative'>
-			<div className='container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28'>
+			<div className='container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-36'>
 				{/* content */}
 				<div className='flex flex-1 flex-col items-center lg:items-start'>
 					<h2 className='text-pet-blue text-3xl md:text-4xl lg:text-5xl text-center lg:text-left mb-6'>
@@ -29,13 +29,12 @@ export const Hero = () => {
 					<Image
 						src={HeroImg}
 						alt='Hero Image'
-						className='w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full'
+						className='w-5/6 h-5/6 sm:w-3/4 md:h-full'
 					/>
 				</div>
 			</div>
 			{/* Rounded Rectangle */}
-			{/* <div className='hidden md:block overflow-hidden bg-pet-indigo rounded-l-full absolute h-80 w-2/4 top-32 right-0'>
-			</div> */}
+			{/* <div className='hidden md:block overflow-hidden bg-pet-indigo rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-36'></div> */}
 		</section>
 	);
 };
