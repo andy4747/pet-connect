f0cdb13306acfb741ae42851ce4ddebcb49ac1e2
Angel Dhakal <angel.dhakal@rts.com.np>
2021-12-23 18:54:12 +0545

    Home Page ✅✅

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FAQ } from './FAQ/FAQ';
+import { Features } from './Features';
+import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { Navbar } from './Navbar';
 
 export const Home = () => {
 	return (
 		<>
-			<Navbar />
-			<Hero />
+			<div className='overflow-hidden'>
+				<Navbar />
+				<Hero />
+				<Features />
+				<FAQ />
+				<Footer />
+			</div>
 		</>
 	);
 };
