a70ea43ab253adf0f70b9226e2be8a28831830ef
Angel Dhakal <angel.dhakal@rts.com.np>
2021-12-23 18:54:59 +0545

    Tailwind config changed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -22,8 +22,38 @@ module.exports = {
 				shadow: '#9D6A6A',
 			},
 		},
+		screens: {
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '640px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		fontFamily: {
 			Poppins: ['Poppins, sans-serif'],
+		},
+		fontSize: {
+			tiny: '0.68rem',
+			xs: '.75rem',
+			sm: '.875rem',
+			base: '1rem',
+			lg: '1.125rem',
+			xl: '1.25rem',
+			'2xl': '1.5rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '5rem',
 		},
 		container: {
 			center: true,
